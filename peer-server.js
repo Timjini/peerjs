4331159852bd86fca01fbfa6779f4abf2e19c242
Timjini <hatim.jini@gmail.com>
2023-08-16 14:23:58 +0300

    first commit

---
+++ peer-server.js
@@ -0,0 +1,17 @@
+// const { ExpressPeerServer } = require('peer');
+// const express = require('express');
+// const http = require('http');
+
+// const app = express();
+// const server = http.createServer(app);
+
+// const peerServer = ExpressPeerServer(server, {
+//   path: '/peer-server', // Specify the path for the Peer server
+// });
+
+// app.use('/peerjs', peerServer); // Use the Peer server middleware
+
+// const PORT = process.env.PORT || 5000; // Define the port for the Peer server
+// server.listen(PORT, () => {
+//   console.log(`Peer server is running on port ${PORT}`);
+// });
