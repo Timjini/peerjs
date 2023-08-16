8e66be79f9a0853e3ea426d258ca6aae1648bb41
Timjini <hatim.jini@gmail.com>
2023-08-16 14:33:39 +0300

    add cors

--- peer-server.js
+++ peer-server.js
@@ -1,17 +1,21 @@
-// const { ExpressPeerServer } = require('peer');
-// const express = require('express');
-// const http = require('http');
+const { ExpressPeerServer } = require('peer');
+const express = require('express');
+const http = require('http');
 
-// const app = express();
-// const server = http.createServer(app);
+const app = express();
+const server = http.createServer(app);
 
-// const peerServer = ExpressPeerServer(server, {
-//   path: '/peer-server', // Specify the path for the Peer server
-// });
+// cors allows us to make requests from the client to the server on a different port
+const cors = require('cors');
+app.use(cors());
 
-// app.use('/peerjs', peerServer); // Use the Peer server middleware
+const peerServer = ExpressPeerServer(server, {
+  path: '/peer-server', // Specify the path for the Peer server
+});
 
-// const PORT = process.env.PORT || 5000; // Define the port for the Peer server
-// server.listen(PORT, () => {
-//   console.log(`Peer server is running on port ${PORT}`);
-// });
+app.use('/peerjs', peerServer); // Use the Peer server middleware
+
+const PORT = process.env.PORT || 5000; // Define the port for the Peer server
+server.listen(PORT, () => {
+  console.log(`Peer server is running on port ${PORT}`);
+});
